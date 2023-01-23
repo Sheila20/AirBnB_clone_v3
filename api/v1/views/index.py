ac4269c3c08bcafb88ac70b367f4c26ea4300e86
Sheila20 <shilanyambura47@gmail.com>
2023-01-23 18:45:32 +0000

    food

---
+++ api/v1/views/index.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Returns JSON status on object app_views
+"""
+from flask import Flask
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/status')
+def status():
+    """ Returns status message in JSON
+    """
+    return ({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """ Returns stats message in JSON
+    """
+    amens = storage.count("Amenity")
+    cities = storage.count("City")
+    places = storage.count("Place")
+    reviews = storage.count("Review")
+    states = storage.count("State")
+    users = storage.count("User")
+
+    return ({"amenities": amens,
+             "cities": cities,
+             "places": places,
+             "reviews": reviews,
+             "states": states,
+             "users": users})
--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -31,8 +31,9 @@ class TestAmenityDocs(unittest.TestCase):
         """Test that tests/test_models/test_amenity.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['tests/test_models/test_amenity.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+        self.assertEqual(result.total_errors, 0
+
+                         "Found code style errors (and warnings)."
 
     def test_amenity_module_docstring(self):
         """Test for the amenity.py module docstring"""
